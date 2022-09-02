ab23cad28dcb4509604a8752f8242133e38d14e9
cNoveron <carlos.escom.ipn@gmail.com>
2022-09-02 18:03:23 -0500

    + payoutToken param

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -16,6 +16,7 @@ contract RockPaperScissors {
         uint256 deadline;
         bytes32 makersChoiceHash;
         string takersChoicePlain;
+        address payoutToken;
     }
 
     mapping(address => TakenBet) currentBet;
@@ -28,15 +29,14 @@ contract RockPaperScissors {
         address maker,
         uint256 deadline,
         bytes32 makersChoiceHash,
-        string takersChoicePlain
+        string takersChoicePlain,
+        address payoutToken
     ) external {
         //require(msg.sender != maker, "take: You can't play against yourself");
         require(block.timestamp < deadline, "Signed transaction expired");
 
         uint chainId;
-        assembly {
-            chainId := chainid
-        }
+        assembly {chainId := chainid()}
         
         bytes32 eip712DomainHash = keccak256(
             abi.encode(
@@ -52,11 +52,12 @@ contract RockPaperScissors {
 
         bytes32 hashStruct = keccak256(
             abi.encode(
-                keccak256("TakenBet(address maker,uint256 deadline,bytes32 makersChoiceHash,string takersChoicePlain)"),
+                keccak256("TakenBet(address maker,uint256 deadline,bytes32 makersChoiceHash,string takersChoicePlain,address payoutToken)"),
                 maker,
                 deadline,
                 makersChoiceHash,
-                takersChoicePlain
+                takersChoicePlain,
+                payoutToken
             )
         );
 
@@ -65,15 +66,15 @@ contract RockPaperScissors {
         require(signer == maker, "take: invalid signature");
         require(signer != address(0), "ECDSA: invalid signature");
 
-        require(currentOpponent[maker].taker == address(0), "take: bet is already taken");
-        currentBet[maker] = TakenBet(msg.sender, deadline, makersChoiceHash, takersChoicePlain);
+        require(currentBet[maker].taker == address(0), "take: bet is already taken");
+        currentBet[maker] = TakenBet(msg.sender, deadline, makersChoiceHash, takersChoicePlain, payoutToken);
         // Now the maker can reveal their bet before the deadline and claim the bet 
     }
 
     function reveal(string makersChoicePlain, string salt) external {
         uint chainId;
         assembly {chainId := chainid()}
-        
+
         bytes32 eip712DomainHash = keccak256(
             abi.encode(
                 keccak256(
@@ -90,6 +91,7 @@ contract RockPaperScissors {
         require(currentBet[msg.sender].makersChoiceHash == hash, "reveal: You didn't chose that move");
 
         string takersChoicePlain = currentBet[msg.sender].takersChoicePlain;
+        address taker = currentBet[msg.sender].taker;
         if (makersChoicePlain == takersChoicePlain) {
             token.transfer(msg.sender, 10 * 1e18);
             token.transfer(currentBet[msg.sender].taker, 10 * 1e18);
