7e732c8fba32ffa3fd4df21bfc780955c9d69f6c
cNoveron <carlos.escom.ipn@gmail.com>
2022-09-08 16:45:37 -0500

    fix ParserError: Expected '(' but got '}'

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -59,13 +59,11 @@ contract ERC20 is Context, IERC20 {
         _symbol = symbol_;
         _decimals = 18;
         uint chainId;
-        assembly {
-            chainId := chainid
-        }
+        assembly { chainId := chainid()}
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
+                keccak256(bytes(name_)),
                 keccak256(bytes('1')),
                 chainId,
                 address(this)
